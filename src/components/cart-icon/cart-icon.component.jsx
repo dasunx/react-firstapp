0baa501aaecec596a0f29ede4a615f054338fcee
Dasunx <97machan@gmail.com>
2020-02-01 19:45:56 +0530

    created cart drop down and connected with cart reducers updated

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,13 +4,19 @@ import './cart-icon.styles.scss';
 import { connect } from 'react-redux';
 import { toggleCartHidden} from '../../redux/cart/cart.actions';
 
-const CartIcon = ({toggleCartHidden}) =>(
+const CartIcon = ({toggleCartHidden, itemCount}) =>(
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'/>
-        <span className='item-count'>0</span>
-    </div>
+        <span className='item-count'>{itemCount}</span>
+    </div> 
 );
+
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: ()=> dispatch(toggleCartHidden())
 }); 
-export default connect(null, mapDispatchToProps)(CartIcon);
+
+const mapStateToProps = ({cart : {cartItems}})=>({
+    itemCount:cartItems.reduce((accumalatedQUantity, cartItem)=> accumalatedQUantity+ cartItem.quantity,0)
+});
+  
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);  
