dce5eaa79a1b6770edca3e5cb3131e0e9acc552d
Dasunx <97machan@gmail.com>
2020-02-05 21:34:00 +0530

    Moved the directory shop data to its own reducer, created respective selectors and updated corresponding components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './preview-collection.style.scss';
+import './collection-preview.style.scss';
 import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({title,items}) => (
--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import { createStructuredSelector} from 'reselect';
-import {selectCollections} from '../../redux/shop/shop.selectors';
+import {selectCollectionsForPreview} from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
-import CollectionPreview from '../preview-collection/preview-collection.component';
+import CollectionPreview from '../collection-preview/collection-preview.component';
 
 const CollectionsOverview = ({collections}) =>(
     <div className='collection-overview'>
@@ -15,7 +15,7 @@ const CollectionsOverview = ({collections}) =>(
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections:selectCollections
+    collections:selectCollectionsForPreview
   });
 
   export default connect(mapStateToProps)(CollectionsOverview);
