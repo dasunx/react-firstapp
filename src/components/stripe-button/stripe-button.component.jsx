d4751d6a5ef0f08a60fdb73eacd31b69d89387ae
Dasunx <97machan@gmail.com>
2020-02-06 21:30:56 +0530

    updating Stripe integration (Test mode only) along with other minor changes

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_sMjTzojPCWnXbl37YpvqoyIP00DvF0CNbv';
+
+    const onToken = token => {
+        console.log(token);
+        alert('payment successful');        
+    }
+
+    return(
+        <StripeCheckout
+            label='Pay Now'
+            name = 'Clothing store'
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/Cuz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    )
+}
+
+
+export default StripeCheckoutButton;
