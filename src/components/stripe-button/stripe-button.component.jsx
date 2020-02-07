59d52b954a7d6a3f66e7dd162d32d84e9f3eb73d
Dasunx <97machan@gmail.com>
2020-02-07 21:50:14 +0530

    remove yarn.lock

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -14,7 +14,7 @@ const StripeCheckoutButton = ({ price }) => {
     return(
         <StripeCheckout
             label='Pay Now'
-            name = 'Clothing store'
+            name = 'KAYA Clothing'   
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/Cuz.svg'
