25545faeaad7aa45ac6d6eeafe5a08faa5dd0172
Dasunx <97machan@gmail.com>
2020-02-12 23:19:57 +0530

    Added the loading screen using Higher order Components (HOC)

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 import CollectionPage from '../collection/collection.component';
 import { updateCollections} from '../../redux/shop/shop.actions';
 import { firestore, convertCollectionsSnapshotToMap} from '../../firebase/firebase.utils';
 
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component{
+  state = {
+    loading: true
+  };
   unsbscribeFromSnapShot = null;
 
   componentDidMount(){
@@ -16,16 +23,17 @@ class ShopPage extends React.Component{
     this.unsbscribeFromSnapShot = collectionRef.onSnapshot(async snapshot =>{
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
-    })
+      this.setState({loading:false});
+    });
   }
 
   render(){
     const { match } = this.props;
-
+    const { loading} = this.state;
     return (
       <div className='shop-page'>
-         <Route exact path={`${match.path}`} component={CollectionsOverview}/>
-         <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+         <Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props}/>}/>
+         <Route path={`${match.path}/:collectionId`} render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props}/>} />
         </div>
       );
   }
