20d9b93650724018ace6df74c4f8c2b07ba4003f
Dasunx <97machan@gmail.com>
2020-02-18 23:05:22 +0530

    Firebase data handling errors are refined

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends React.Component {
     const {setCurrentUser} = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      if(userAuth){
+      if(userAuth){ 
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot=>{
