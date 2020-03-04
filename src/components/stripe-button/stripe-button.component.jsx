451a7dc9ac693fdf4735718a2a577568d429e461
Dasunx <97machan@gmail.com>
2020-03-04 21:34:48 +0530

    Refined payment to clear cart after paying successfully

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,31 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import StripeCheckout from 'react-stripe-checkout';
+import { clearCartAfterPayment } from '../../redux/cart/cart.actions'
 
 
-const StripeCheckoutButton = ({ price }) => {
+const StripeCheckoutButton = ({ price, clearCartFn }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_sMjTzojPCWnXbl37YpvqoyIP00DvF0CNbv';
 
     const onToken = token => {
         console.log(token);
-        alert('payment successful');        
+              
     }
 
-    return(
+    return (
         <StripeCheckout
             label='Pay Now'
-            name = 'KAYA Clothing'   
+            name='KAYA Clothing'
             billingAddress
             shippingAddress
             image='https://svgshare.com/i/Cuz.svg'
             description={`Your total is $${price}`}
             amount={priceForStripe}
             panelLabel='Pay Now'
-            token={onToken}
+            token={clearCartFn}
             stripeKey={publishableKey}
         />
     )
 }
 
+const mapDispatchToProps = dispatch => ({
+    clearCartFn: () => dispatch(clearCartAfterPayment())
+})
 
-export default StripeCheckoutButton;
+
+export default connect(null, mapDispatchToProps)(StripeCheckoutButton);
